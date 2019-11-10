5fcc6f8c6d0dbc5c47ffb7527a66bd46ba97d30f
Patriot <patriotrossii2019@mail.ru>
2019-11-10 19:11:06 +0600

    Revert "Add getter functions, change constructor, remove unnecessary includes"
    This reverts commit 1458b688cad95b476b8d8295cb916f21bbf22d4c.

--- include/VkApi.h
+++ include/VkApi.h
@@ -7,6 +7,8 @@
 #include "Poco/Net/HTTPSClientSession.h"
 #include "Poco/URI.h"
 
+#include <string_view>
+
 class VkApi {
 private:
     Poco::Net::HTTPSClientSession session;
@@ -14,8 +16,13 @@ private:
     Poco::Net::HTTPResponse response;
     Poco::URI uri;
     Poco::JSON::Parser parser;
+    
+    const std::string access_token;
+    const std::string version;
 public:
-    VkApi();
+    VkApi(std::string token, std::string version_);
     Poco::JSON::Object::Ptr executeMethod(const std::string&,
-                                          const Poco::URI::QueryParameters&);
+                                          const Poco::URI::QueryParameters& params={});
+    const std::string& getVersion() const;
+    const std::string& getToken() const;
 };
--- sources/VkApi.cpp
+++ sources/VkApi.cpp
@@ -1,19 +1,31 @@
 #include "../include/VkApi.h"
 
-VkApi::VkApi()
+VkApi::VkApi(const std::string token, const std::string version_)
     : session("api.vk.com", 443, new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "", "",
                                         "", Poco::Net::Context::VERIFY_NONE, 9,
                                         false,
-                                        "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH")), request(Poco::Net::HTTPMessage::HTTP_1_1) {
+                                        "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH")), request(Poco::Net::HTTPMessage::HTTP_1_1), access_token(std::move(token)), version(std::move(version_)) {
     request.setMethod(Poco::Net::HTTPRequest::HTTP_GET);
 }
 
 Poco::JSON::Object::Ptr VkApi::executeMethod(
     const std::string& method, const Poco::URI::QueryParameters& params) {
     uri = "https://api.vk.com/method/" + method;
-    uri.setQueryParameters(params);
+    if(params.size()) {
+        uri.setQueryParameters(params);
+    }
+    uri.addQueryParameter("access_token", access_token);
+    uri.addQueryParameter("v", version);
     request.setURI(uri.toString());
     session.sendRequest(request);
     return parser.parse(session.receiveResponse(response))
         .extract<Poco::JSON::Object::Ptr>();
+}
+
+const std::string& VkApi::getVersion() const {
+    return version;
+}
+
+const std::string& VkApi::getToken() const {
+    return token;
 }
