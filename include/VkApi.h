ad9d7b1677f9f0de11d03f05c7e5333f14e3eb6a
Patriot <patriotrossii2019@mail.ru>
2019-11-22 22:24:15 +0600

    Now library is header-only!

--- include/VkApi.h
+++ include/VkApi.h
@@ -25,13 +25,56 @@ private:
     std::string access_token;
     std::string version;
 public:
-    VkApi();
-    VkApi(std::string token);
-    VkApi(std::string login, std::string password);
-    VkApi(std::string login, std::string password, std::string token, std::string version);
-    bool auth();
-    nlohmann::json executeMethod(const std::string&, const Poco::URI::QueryParameters& params={});
+    VkApi() : VkApi("", "", "", DEFAULT_VERSION) { }
     
+    template <class T1>
+    VkApi(T1&& token) : VkApi("", "", std::forward<T1>(token), DEFAULT_VERSION) { }
+    
+    template <class T1, class T2>
+    VkApi(T1&& login, T2&& password) : VkApi(std::forward<T1>(login), std::forward<T2>(password), "", DEFAULT_VERSION) { }
+    
+    template<class T1, class T2, class T3, class T4>
+    VkApi(T1&& login, T2&& password, T3&& token, T4&& version) : session("vk.com", 443, new Poco::Net::Context(Poco::Net::Context::CLIENT_USE, "", "", "", Poco::Net::Context::VERIFY_NONE, 9, false, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH")), request(Poco::Net::HTTPMessage::HTTP_1_1), login(std::forward<T1>(login)), password(std::forward<T2>(password)), access_token(std::forward<T3>(token)), version(std::forward<T4>(version)) {
+        request.setMethod(Poco::Net::HTTPRequest::HTTP_GET);
+    }
+
+    nlohmann::json executeMethod(const std::string& method, const Poco::URI::QueryParameters& params={}) {
+        uri = "https://api.vk.com/method/" + method;
+        if(params.size()) {
+            uri.setQueryParameters(params);
+        }
+        uri.addQueryParameter("access_token", access_token);
+        uri.addQueryParameter("v", version);
+        request.setURI(uri.toString());
+        
+        session.sendRequest(request);
+        
+        nlohmann::json result;
+        session.receiveResponse(response) >> result;
+        
+        return result;
+    }
+                           
+                                          
+    bool auth() {
+        if(!login.empty() && !password.empty() && access_token.empty()) {
+            uri = "https://oauth.vk.com/token";
+            uri.setQueryParameters({{"grant_type", "password"}, {"client_id", CLIENT_ID}, {"client_secret", CLIENT_SECRET}, {"username", login}, {"password", password}});
+            request.setURI(uri.toString());
+            
+            session.sendRequest(request);
+            
+            nlohmann::json result;
+            session.receiveResponse(response) >> result;
+            
+            if(result.count("access_token")) {
+                this->access_token = result["access_token"].get<std::string>();
+                return true;
+            }
+        }
+        return false;
+    }
+
     const std::string& getVersion() const {
         return version;
     }
@@ -46,7 +89,7 @@ public:
     }
     
     template <class T>
-    void setVersion(T& version) {
+    void setVersion(T&& version) {
         this->version = std::forward<T>(version);
     }
 };
